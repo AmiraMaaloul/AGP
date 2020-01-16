b0777a5cd965ee522752bf291615d3186a0427df
mac <mac@10.77.21.116>
2020-01-16 12:09:32 +0100

    change entry page and entry bean

--- src/beans/EntryBean.java
+++ src/beans/EntryBean.java
@@ -1,6 +1,49 @@
 package beans;
 
-	
+
 public class EntryBean {
 		
+	     
+	    private String keyword;
+	    private int pricemin;
+	    private int pricemax;
+	    private String confort;
+	    private int nbday;
+		public String getKeyword() {
+			return keyword;
+		}
+		public void setKeyword(String keyword) {
+			this.keyword = keyword;
+		}
+		public int getPricemin() {
+			return pricemin;
+		}
+		public void setPricemin(int pricemin) {
+			this.pricemin = pricemin;
+		}
+		public int getPricemax() {
+			return pricemax;
+		}
+		public void setPricemax(int pricemax) {
+			this.pricemax = pricemax;
+		}
+		public String getConfort() {
+			return confort;
+		}
+		public void setConfort(String confort) {
+			this.confort = confort;
+		}
+		public int getNbday() {
+			return nbday;
+		}
+		public void setNbday(int nbday) {
+			this.nbday = nbday;
+		}
+
+
+
+	 
+	   
+	
+	
 }
