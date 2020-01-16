3cf9e01f63b2447cf40690ce7a2f538533e4b02e
fares <fares@DESKTOP-69V9HAJ>
2020-01-17 00:03:26 +0100

    correction jsf

--- src/beans/EntryBean.java
+++ src/beans/EntryBean.java
@@ -1,16 +1,23 @@
 package beans;
 
-import java.io.Serializable;
-
-import javax.annotation.ManagedBean;
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+
+
+/**
+ * Simulation bean controller used to collect simulation entry parameters and to start the simulation.
+ * 
+ * The proxy design pattern is used for avoiding redundant code copy.
+ */
 @ManagedBean
 @SessionScoped
-public class EntryBean implements Serializable {
+public class EntryBean {
 
-	private static final long serialVersionUID = -426721429642192283L;
-
+	/**
+	 * Proxy encapsulated object. All get/set of parameters work on this proxy object.
+	 */
+	
 	/* For Offers */
 	private String offerkeyword;
 	private String offerpricemin;
@@ -29,128 +36,98 @@ public class EntryBean implements Serializable {
 	private String sitePriceMin;
 	private String sitePriceMax;
 	private String siteType;
-
 	public EntryBean() {
-
 	}
-
 	public String getOfferkeyword() {
 		return offerkeyword;
 	}
-
 	public void setOfferkeyword(String offerkeyword) {
 		this.offerkeyword = offerkeyword;
 	}
-
 	public String getOfferpricemin() {
 		return offerpricemin;
 	}
-
 	public void setOfferpricemin(String offerpricemin) {
 		this.offerpricemin = offerpricemin;
 	}
-
 	public String getOfferpricemax() {
 		return offerpricemax;
 	}
-
 	public void setOfferpricemax(String offerpricemax) {
 		this.offerpricemax = offerpricemax;
 	}
-
 	public String getOffernbday() {
 		return offernbday;
 	}
-
 	public void setOffernbday(String offernbday) {
 		this.offernbday = offernbday;
 	}
-
 	public String getOfferconfort() {
 		return offerconfort;
 	}
-
-	public void setOfferCconfort(String offerconfort) {
+	public void setOfferconfort(String offerconfort) {
 		this.offerconfort = offerconfort;
 	}
-
 	public String getHotelPriceMax() {
 		return hotelPriceMax;
 	}
-
 	public void setHotelPriceMax(String hotelPriceMax) {
 		this.hotelPriceMax = hotelPriceMax;
 	}
-
 	public String getHotelPriceMin() {
 		return hotelPriceMin;
 	}
-
 	public void setHotelPriceMin(String hotelPriceMin) {
 		this.hotelPriceMin = hotelPriceMin;
 	}
-
 	public String getHotelName() {
 		return hotelName;
 	}
-
 	public void setHotelName(String hotelName) {
 		this.hotelName = hotelName;
 	}
-
 	public String getRating() {
 		return rating;
 	}
-
 	public void setRating(String rating) {
 		this.rating = rating;
 	}
-
 	public String getSiteName() {
 		return siteName;
 	}
-
 	public void setSiteName(String siteName) {
 		this.siteName = siteName;
 	}
-
 	public String getSitePriceMin() {
 		return sitePriceMin;
 	}
-
 	public void setSitePriceMin(String sitePriceMin) {
 		this.sitePriceMin = sitePriceMin;
 	}
-
 	public String getSitePriceMax() {
 		return sitePriceMax;
 	}
-
 	public void setSitePriceMax(String sitePriceMax) {
 		this.sitePriceMax = sitePriceMax;
 	}
-
 	public String getSiteType() {
 		return siteType;
 	}
-
 	public void setSiteType(String siteType) {
 		this.siteType = siteType;
 	}
 
-	public String searchhotels() {
 
-		return "HotelResult";
-	}
-
-	public String searchoffers() {
-
-		return "OfferResult";
-	}
-
-	public String searchsites() {
-
-		return "SiteResult";
-	}
+	
+	
+	
 
 }
+
+
+
+
+
+
+
+
